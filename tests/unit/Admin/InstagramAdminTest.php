830688e9bdb1d0b8f7290e16433cf6d933fa9ac3
Michael Nowina-Krowicki <michael@3dgoo.com>
2020-06-04 12:26:29 +0930

    Test tweak

--- tests/unit/Admin/InstagramAdminTest.php
+++ tests/unit/Admin/InstagramAdminTest.php
@@ -4,6 +4,7 @@ namespace X3dgoo\InstagramScraper\Tests;
 
 use SilverStripe\Dev\FunctionalTest;
 use X3dgoo\InstagramScraper\Admin\InstagramAdmin;
+use X3dgoo\InstagramScraper\Model\InstagramPost;
 
 class InstagramAdminTest extends FunctionalTest
 {
@@ -15,6 +16,13 @@ class InstagramAdminTest extends FunctionalTest
 
         $instagramAdmin = InstagramAdmin::singleton();
         $response = $this->get($instagramAdmin->Link());
+
+        $instagramPost1 = InstagramPost::create();
+        $instagramPost1->ImageThumbnailURL = 'https://www.google.com/image.png';
+        $instagramPost1->write();
+
+        $instagramPost2 = InstagramPost::create();
+        $instagramPost2->write();
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertPartialMatchBySelector(
